8ecaf66754d397bd0ac233ba9956246a38e85341
jehovany04 <7528@holbertonstudents.com>
2023-12-21 14:32:29 -0800

    main.h

--- shell.c
+++ shell.c
@@ -1,26 +1,38 @@
 #include "main.h"
 
-void executer_commande(char *line)
+/**
+*executer_commande - Execute une commande en créant un processus enfant.
+*
+* @param : line La commande à exécuter.
+*/
 
+void executer_commande(char *line)
 {
-    pid_t pid_enfant;
-    int status;
-    pid_enfant = fork();
-    if (pid_enfant == -1)
-    {
-        perror_exit("Erreur création processus enfant");
-        exit(EXIT_FAILURE);
-    }
-    if (pid_enfant == 0)  /*Processus enfant*/
-    {
-        if (execve(line, NULL, environ) == -1)
-        {
-            perror_exit("Commande introuvable");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else  /*Processus parent*/
-    {
-        waitpid(pid_enfant, &status, 0);
-    }
+	pid_t pid_enfant;
+	int status;
+
+	pid_enfant = fork();
+
+	if (pid_enfant == -1)
+	{
+		perror_exit("Erreur création processus enfant");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid_enfant == 0)  /*Processus enfant*/
+	{
+		char *argv[2];
+		argv[0] = line;
+		argv[1] = NULL;
+
+		if (execve(line, argv, environ) == -1)
+		{
+			perror_exit("Commande introuvable");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else  /*Processus parent*/
+	{
+		waitpid(pid_enfant, &status, 0);
+	}
 }
